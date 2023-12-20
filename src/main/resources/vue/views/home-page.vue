182d6bf4fd0c16948ed5224aa532eb49a05d02f7
Madhan <madhan12n@gmail.com>
2023-12-20 10:19:42 +0000

    Create Home Page UI

---
+++ src/main/resources/vue/views/home-page.vue
@@ -0,0 +1,13 @@
+<!-- the "home-page" element is passed as a parameter to VueComponent in the JavalinConfig file -->
+<template id="home-page">
+ <app-layout>
+    <h1> Welcome to Health Tracker App</h1>
+  </app-layout>
+</template>
+
+<script>
+app.component('home-page',
+    {
+      template: "#home-page",
+    });
+</script>

e2a8a8403db685511b64dcd8be3a40f0b5488aa3
Madhan <madhan12n@gmail.com>
2023-12-21 11:01:41 +0000

    Display Activity list

---
+++ src/main/resources/vue/views/activity-overview.vue
@@ -0,0 +1,29 @@
+<template id="activity-overview">
+  <div>
+    <div>
+      <ul class="activity-overview-list">
+        <li v-for="activity in activities">
+          {{activity.description}}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+app.component("activity-overview", {
+  template: "#activity-overview",
+  data: () => ({
+    activities: [],
+  }),
+  created() {
+    this.fetchActivities();
+  },
+  methods: {
+    fetchActivities: function () {
+      axios.get("/api/activities")
+          .then(res => this.activities = res.data)
+          .catch(() => alert("Error while fetching users"));
+    }
+  }
+});
+</script>

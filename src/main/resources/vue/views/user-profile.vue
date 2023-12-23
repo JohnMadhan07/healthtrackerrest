6390252211519ece50e46600dfefd80bfc0e38e2
Madhan <madhan12n@gmail.com>
2023-12-23 21:41:26 +0000

    Add a Card for User Profile view

--- src/main/resources/vue/views/user-profile.vue
+++ src/main/resources/vue/views/user-profile.vue
@@ -1,20 +1,25 @@
 <template id="user-profile">
-  <div>
-    <form v-if="user">
-      <label class="col-form-label">User ID: </label>
-      <input class="form-control" v-model="user.id" name="id" type="number" readonly/><br>
-      <label class="col-form-label">Name: </label>
-      <input class="form-control" v-model="user.name" name="username" type="text"/><br>
-      <label class="col-form-label">Email: </label>
-      <input class="form-control" v-model="user.email" name="email" type="email"/><br>
-    </form>
-    <dt v-if="user">
-      <br>
-      <a :href="`/users/${user.id}/activities`">View User Activities</a>
-    </dt>
-  </div>
+  <app-layout>
+    <div class="card bg-light mb-3">
+      <div class="card-header">
+        User Profile
+      </div>
+      <div class="card-body">
+        <form v-if="user">
+          <label class="col-form-label">User ID: </label>
+          <input class="form-control" v-model="user.id" name="id" type="number" readonly/><br>
+          <label class="col-form-label">Name: </label>
+          <input class="form-control" v-model="user.name" name="username" type="text"/><br>
+          <label class="col-form-label">Email: </label>
+          <input class="form-control" v-model="user.email" name="email" type="email"/><br>
+          <div v-if="user">
+            <a :href="`/users/${user.id}/activities`">View User Activities</a>
+          </div>
+        </form>
+      </div>
+    </div>
+  </app-layout>
 </template>
-
 <script>
 app.component("user-profile", {
   template: "#user-profile",

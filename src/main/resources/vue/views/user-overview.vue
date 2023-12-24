41e48b11ee5a57b43fa56b548709a3ebf9c621fa
Madhan <madhan12n@gmail.com>
2023-12-24 20:15:47 +0000

    Change list of users view in User Overview

--- src/main/resources/vue/views/user-overview.vue
+++ src/main/resources/vue/views/user-overview.vue
@@ -1,13 +1,14 @@
 <template id="user-overview">
-  <div>
-    <div>
-      <ul class="user-overview-list">
-        <li v-for="user in users">
-          <a :href="`/users/${user.id}`">{{user.name}} ({{user.email}})</a>
-        </li>
-      </ul>
+  <app-layout>
+    <div class="list-group list-group-flush">
+      <div class="list-group-item d-flex align-items-start"
+           v-for="(user,index) in users" v-bind:key="index">
+        <div class="mr-auto p-2">
+          <span><a :href="`/users/${user.id}`"> {{ user.name }} ({{ user.email }})</a></span>
+        </div>
+      </div>
     </div>
-  </div>
+  </app-layout>
 </template>
 <script>
 app.component("user-overview", {

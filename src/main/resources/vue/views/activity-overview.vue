40f185bd2c3cf8069ad9157e821b7ed470490a5f
Madhan <madhan12n@gmail.com>
2023-12-28 13:25:07 +0000

    Create Add Activity button

--- src/main/resources/vue/views/activity-overview.vue
+++ src/main/resources/vue/views/activity-overview.vue
@@ -6,12 +6,45 @@
           <div class="col-6">
             Activities
           </div>
+          <div class="col" align="right">
+            <button rel="tooltip" title="Add"
+                    class="btn btn-info btn-simple btn-link"
+                    @click="hideForm =!hideForm">
+              <i class="fa fa-plus" aria-hidden="true"></i>
+            </button>
+          </div>
         </div>
       </div>
     </div>
+    <div class="card-body" :class="{ 'd-none': hideForm}">
+      <form id="addActivity">
+        <!-- Assuming user ID is entered by the user -->
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="input-activity-userId">User ID</span>
+          </div>
+          <input type="text" class="form-control" v-model="formData.userId" name="userId" placeholder="User ID"/>
+        </div>
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="input-activity-description">Description</span>
+          </div>
+          <input type="text" class="form-control" v-model="formData.description" name="name" placeholder="Description"/>
+        </div>
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="input-activity-duration">Duration</span>
+          </div>
+          <input type="text" class="form-control" v-model="formData.duration" name="duration" placeholder="Duration in mins"/>
+        </div>
+      </form>
+      <button rel="tooltip" title="Update" class="btn btn-info btn-simple btn-link" @click="addActivity()">
+        Add Activity
+      </button>
+    </div>
     <div class="list-group list-group-flush">
       <div class="list-group-item d-flex align-items-start"
-           v-for="(activity,index) in activities" v-bind:key="index">
+           v-for="(activity,index) in activities" :key="index">
         <div class="mr-auto p-2">
           <span><a :href="`/activities/${activity.id}`"> {{ activity.description }}</a></span>
         </div>
@@ -24,6 +57,8 @@ app.component("activity-overview", {
   template: "#activity-overview",
   data: () => ({
     activities: [],
+    formData: [],
+    hideForm: true
   }),
   created() {
     this.fetchActivities();
@@ -32,7 +67,23 @@ app.component("activity-overview", {
     fetchActivities: function () {
       axios.get("/api/activities")
           .then(res => this.activities = res.data)
-          .catch(() => alert("Error while fetching users"));
+          .catch(() => alert("Error while fetching activities"));
+    },
+    addActivity: function () {
+      const url = `/api/activities`;
+      axios.post(url,
+          {
+            description: this.formData.description,
+            duration: this.formData.duration
+          })
+          .then(response => {
+            this.activities.push(response.data)
+            this.hideForm= true;
+            window.location.href = '/activities';
+          })
+          .catch(error => {
+            console.log(error)
+          })
     }
   }
 });
